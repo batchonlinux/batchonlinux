7e3cee6a4feb96864d00921532d7d18fcc51796c
Andris <andris5040@gmail.com>
2025-05-29 21:07:46 +0200

    Update utils.rb

--- utils.rb
+++ utils.rb
@@ -4,7 +4,6 @@ module Utils
       raise "No batch file provided" if args.empty?
       batch_file = args.first
 
-      # Ellenőrizzük, hogy a fájl kiterjesztése .bat legyen
       raise "Provided file '#{batch_file}' is not a batch file (.bat)" unless batch_file.end_with?('.bat')
 
       batch_file
