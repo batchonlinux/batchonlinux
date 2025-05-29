cdbb56f26e4e8065a64620f116e2e774d870a962
Andris <andris5040@gmail.com>
2025-05-29 21:08:02 +0200

    Update wine_executor.rb

--- wine_executor.rb
+++ wine_executor.rb
@@ -2,10 +2,8 @@ module WineExecutor
   def self.run(batch_file)
     begin
       puts "Running batch file '#{batch_file}' using Wine..."
-      # Ellenőrizzük, hogy létezik-e a wine
       raise "Wine is not installed or not in the PATH" unless system("which wine > /dev/null 2>&1")
 
-      # Próbálkozunk a batch fájl futtatásával
       success = system("wine cmd /c #{batch_file}")
       raise "Failed to execute batch file with Wine" unless success
     rescue => e
