307d47d5fd74c037cfe11bdd2b5c37df99e58878
Andris <andris5040@gmail.com>
2025-05-29 21:05:59 +0200

    Update batchonlinux.rb

--- batchonlinux.rb
+++ batchonlinux.rb
@@ -2,10 +2,8 @@ require_relative '../lib/batch_runner'
 require_relative '../lib/utils'
 
 begin
-  # Parancssori argumentumok feldolgozása
   batch_file = Utils.parse_arguments(ARGV)
   
-  # Batch fájl futtatása
   BatchRunner.run(batch_file)
 rescue => e
   puts "Error: #{e.message}"
